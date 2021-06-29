f0149ea2da8af44dafa086cea8be827675915296
mieubrisse <mieubrisse@gmail.com>
2021-06-29 15:35:17 -0500

    Make ServiceIdIpReplacementPrefix/Suffix public

--- golang/bulk_command_execution/v0_bulk_command_api/v0_service_ip_replacement.go
+++ golang/bulk_command_execution/v0_bulk_command_api/v0_service_ip_replacement.go
@@ -1,15 +1,15 @@
 package v0_bulk_command_api
 
 const (
-	serviceIdIpReplacementPrefix = "<<<"
-	serviceIdIpReplacementSuffix = ">>>"
+	ServiceIdIpReplacementPrefix = "<<<"
+	ServiceIdIpReplacementSuffix = ">>>"
 
 	// The *? indicates a non-greedy match
-	ServiceIdIpReplacementPattern = serviceIdIpReplacementPrefix + ".*?" + serviceIdIpReplacementSuffix
+	ServiceIdIpReplacementPattern = ServiceIdIpReplacementPrefix + ".*?" + ServiceIdIpReplacementSuffix
 )
 
 // Used to encode a service ID to a string that can be embedded in commands, and which the API container will replace
 //  with the IP address of the service at runtime
 func EncodeServiceIdForIpReplacement(serviceId string) string {
-	return serviceIdIpReplacementPrefix + serviceId + serviceIdIpReplacementSuffix
+	return ServiceIdIpReplacementPrefix + serviceId + ServiceIdIpReplacementSuffix
 }
