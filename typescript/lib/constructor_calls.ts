c703e50526d4ec6a6d624bc4d75410d0a19201eb
Ali Syed <alisyed0206@gmail.com>
2021-07-23 18:21:24 -0400

    iterating on Kevin feedback: type assertions, eject early + often, etc..

--- typescript/lib/constructor_calls.ts
+++ typescript/lib/constructor_calls.ts
@@ -1,17 +1,17 @@
-import * as bindingsJs from '../kurtosis_core_rpc_api_bindings/api_container_service_pb';
+import { ExecCommandArgs, GenerateFilesArgs, FileGenerationOptions, LoadStaticFilesArgs } from '../kurtosis_core_rpc_api_bindings/api_container_service_pb';
 import { ServiceID } from './services/service';
 
 
-export function newGetExecCommandArgs(serviceId: ServiceID, command: string[]): bindingsJs.ExecCommandArgs {
-    const result = new bindingsJs.ExecCommandArgs();
+export function newGetExecCommandArgs(serviceId: ServiceID, command: string[]): ExecCommandArgs {
+    const result = new ExecCommandArgs();
     result.setServiceId(serviceId);
     result.setCommandArgsList(command);
 
     return result;
 }
 
-export function newGetGenerateFilesArgs(serviceId: ServiceID, fileGenerationOpts: Map<string, bindingsJs.FileGenerationOptions>): bindingsJs.GenerateFilesArgs {
-    const result = new bindingsJs.GenerateFilesArgs();
+export function newGetGenerateFilesArgs(serviceId: ServiceID, fileGenerationOpts: Map<string, FileGenerationOptions>): GenerateFilesArgs {
+    const result = new GenerateFilesArgs();
     result.setServiceId(String(serviceId)); 
     for (let fileID in fileGenerationOpts) { //TODO - changing the following to a const, I'm also certain we don't
         result.getFilesToGenerateMap().set(fileID, fileGenerationOpts[fileID]);
@@ -20,8 +20,18 @@ export function newGetGenerateFilesArgs(serviceId: ServiceID, fileGenerationOpts
     return result;
 }
 
-export function newGetLoadStaticFilesArgs(serviceId: ServiceID, staticFilesToCopyStringSet: Map<String, boolean>): bindingsJs.LoadStaticFilesArgs {
-    const result = new bindingsJs.LoadStaticFilesArgs();
+export function newGetFileGenerationOptions(filesToGenerateSet: Set<string>): Map<string, FileGenerationOptions> {
+    const result: Map<string, FileGenerationOptions> = new Map();
+    for (let fileId in filesToGenerateSet) { // TODO - should I change it to const
+        result[fileId] = new FileGenerationOptions();
+        result[fileId].setFileTypeToGenerate(FileGenerationOptions.FileTypeToGenerate.FILE); //TODO (comment) - incosistency - brackets needed = I don't think so since we need to since we only to call FILE
+    }
+
+    return result;
+}
+
+export function newGetLoadStaticFilesArgs(serviceId: ServiceID, staticFilesToCopyStringSet: Map<String, boolean>): LoadStaticFilesArgs {
+    const result = new LoadStaticFilesArgs();
     result.setServiceId(String(serviceId));
     const staticFilesMap = result.getStaticFilesMap();
     for (let staticFildID in staticFilesToCopyStringSet) { //TODO - changing the following to a const, I'm also certain we don't
--- typescript/lib/services/service_context.ts
+++ typescript/lib/services/service_context.ts
@@ -1,10 +1,10 @@
 import { ApiContainerServiceClient } from '../../kurtosis_core_rpc_api_bindings/api_container_service_grpc_pb'; 
-import { ExecCommandResponse, FileGenerationOptions, GenerateFilesResponse, LoadStaticFilesResponse } from '../../kurtosis_core_rpc_api_bindings/api_container_service_pb';
+import { ExecCommandArgs, ExecCommandResponse, FileGenerationOptions, GenerateFilesArgs, GenerateFilesResponse, LoadStaticFilesArgs, LoadStaticFilesResponse } from '../../kurtosis_core_rpc_api_bindings/api_container_service_pb';
 import { ServiceID } from './service';
 import { StaticFileID } from './container_creation_config'; 
 import * as grpc from "grpc"; 
-import * as constructors from "../constructor_calls";
-var path = require("path"); //TODO - I don't think this works
+import { newGetExecCommandArgs, newGetGenerateFilesArgs, newGetFileGenerationOptions, newGetLoadStaticFilesArgs } from "../constructor_calls";
+var path = require("path"); //TODO - I don't think this works, but it has been installed with grpc
 
 
 // Docs available at https://docs.kurtosistech.com/kurtosis-libs/lib-documentation
@@ -45,86 +45,79 @@ class ServiceContext {
     }
 
     // Docs available at https://docs.kurtosistech.com/kurtosis-libs/lib-documentation
-    public execCommand(command: string[]): [number, [], Error] { //TODO - [int32, pointer to []byte (comment), error ] ; is is not []`type` like []string
-        const serviceId = this.serviceId;
-        const args = constructors.newGetExecCommandArgs(serviceId, command);
+    public execCommand(command: string[]): [number, Uint8Array | string, Error] { //TODO In golang: [int32, pointer to []byte (comment), error ] ; Following types in _pb.d.ts
+        const serviceId: ServiceID = this.serviceId;
+        const args: ExecCommandArgs = newGetExecCommandArgs(serviceId, command);
         let request: grpc.requestCallback<ExecCommandResponse>; //TODO - Tried to implement callback through examples on Google while also respecting the actual Callback function inside grpc ; (comment) need to initalize if want to change it to const
 
-        let resp, err = this.client.execCommand(args, request); //TODO - does error checking work here ; check the return type of execCommand, it's not an ExecCommandResponse ? ; (comment) need to initalize if want to change it to const
+        let resp, err = this.client.execCommand(args, request); //TODO - does error checking work here ; check the return type of execCommand, it's not an ExecCommandResponse ? ; (comment) need to initalize if want to change it to const ; try adding type assertion
         
-        if (err != null) {
+        if (err !== null) {
             return [0, null, new Error( //TODO - Error type (do I need callback?)
 			    "An error occurred executing command '%v' on service '%v'", //TODO - I might not be able keep the '%v', but how I throw the actual error
-            )]; //TODO (comment) cannot return multipe values in typescript function, need to wrap in array or object
+            )];
         }
-        return [resp.ExitCode, resp.LogOutput, null]; //TODO need to make sure resp is ExecCommandResponse if I want to return this ; (comment) returning multiple values as an array
+        return [resp.ExitCode, resp.LogOutput, null]; //TODO need to make sure resp is ExecCommandResponse if I want to return this
     }
 
     // Docs available at https://docs.kurtosistech.com/kurtosis-libs/lib-documentation
     public generateFiles(filesToGenerateSet: Set<string>): [Map<string, GeneratedFileFilepaths>, Error] {
-        const serviceId = this.serviceId;
-        const fileGenerationOpts = new Map(); //TODO (comment) Key: string & Value: core_api_bindings.FileGenerationOptions{}
-        for (let fileId in filesToGenerateSet) { // TODO - should I change it to const
-            fileGenerationOpts[fileId] = new FileGenerationOptions();
-            fileGenerationOpts[fileId].setFileTypeToGenerate(FileGenerationOptions.FileTypeToGenerate.FILE); //TODO (comment) - incosistency - brackets needed = I don't think so since we need to since we only to call FILE
-        }
+        const serviceId: ServiceID = this.serviceId;
+        const fileGenerationOpts: Map<string, FileGenerationOptions> = newGetFileGenerationOptions(filesToGenerateSet);
 
-        const args = constructors.newGetGenerateFilesArgs(serviceId, fileGenerationOpts);
+        const args: GenerateFilesArgs = newGetGenerateFilesArgs(serviceId, fileGenerationOpts);
         let request: grpc.requestCallback<GenerateFilesResponse>; // TODO (comment) need to initalize if want to change it to const
 
-        let resp, err = this.client.generateFiles(args, request); //TODO - returns grpc.ClientUnaryCall, doesn't directly return error & not possible to return multiple values directly from function ; (comment) need to initalize if want to change it to const
-        if (err != null){
+        let resp, err = this.client.generateFiles(args, request); //TODO - returns grpc.ClientUnaryCall, doesn't directly return error & not possible to return multiple values directly from function ; (comment) need to initalize if want to change it to const, type assertion
+        if (err !== null){
             return [null, new Error("An error occurred generating files using args: %+v")]; //TODO - no %+v maybe, but should be throwing actual error
         }
-        const generatedFileRelativeFilepaths = resp.GeneratedFileRelativeFilepaths;
+        const generatedFileRelativeFilepaths: Map<string, string> = resp.GeneratedFileRelativeFilepaths;
 
-        const result = new Map(); // TODO Key:string & Value:GeneratedFileFilepaths{} - type not needed in typescript
+        const result: Map<string, GeneratedFileFilepaths> = new Map();
         for (let fileId in filesToGenerateSet) { //TODO - should I change to const
-            //TODO - need to add check if fileID is inside map so that it it either manipulates the map or sens an error
-            var relativeFilepath;
-            if (generatedFileRelativeFilepaths.has(fileId)) {
-                relativeFilepath = generatedFileRelativeFilepaths[fileId];
-            }
-            else {
+            
+            var relativeFilepath: string;
+            if (!generatedFileRelativeFilepaths.has(fileId)) {
                 return [null, new Error(
                     "No filepath (relative to test volume root) was returned for file '%v', even though we requested it; this is a Kurtosis bug", //TODO - no %v maybe
                     )];
             }
-            // let absFilepathOnTestsuite = new URL(this.testVolumeMountpointOnTestsuiteContainer, relativeFilepath); //TODO need to confirm this VS path
-            // let absFilepathOnService = new URL(this.testVolumeMountpointOnServiceContainer, relativeFilepath);
-            const absFilepathOnTestsuite = path.Join(this.testVolumeMountpointOnTestsuiteContainer, relativeFilepath);
-            const absFilepathOnService = path.Join(this.testVolumeMountpointOnServiceContainer, relativeFilepath);
-            const genFilePath = new GeneratedFileFilepaths(); //TODO = this might be a problem in terms of memory (direct or reference), so check
+            relativeFilepath = generatedFileRelativeFilepaths[fileId];
+
+            const absFilepathOnTestsuite: string = path.Join(this.testVolumeMountpointOnTestsuiteContainer, relativeFilepath);
+            const absFilepathOnService: string = path.Join(this.testVolumeMountpointOnServiceContainer, relativeFilepath);
+            const genFilePath: GeneratedFileFilepaths = new GeneratedFileFilepaths();
+
             genFilePath.absoluteFilepathOnTestsuiteContainer = absFilepathOnTestsuite;
             genFilePath.absoluteFilepathOnServiceContainer = absFilepathOnService;
             result.set(fileId, genFilePath); 
-            //}
         }
-        return [result, null]; //TODO - Design decision, returning multiple values as an array
+        return [result, null];
     }
 
     // Docs available at https://docs.kurtosistech.com/kurtosis-libs/lib-documentation
     public loadStaticFiles(usedStaticFilesSet: Set<StaticFileID>): [Map<StaticFileID, string>, Error] { // TODO (comment) - changed Map into Set; possible to return useful erorr messages in typescript
-        const serviceId = this.serviceId;
-        const staticFilesToCopyStringSet = new Map(); //TODO (comment) = map[string]bool{} ; kept this as a map sinc call to loadStaticFile has map property that requires a boolean
+        const serviceId: ServiceID = this.serviceId;
+        const staticFilesToCopyStringSet: Map<string, boolean> = new Map(); 
         for (let staticFileId in usedStaticFilesSet) { //TODO - change to const?
             staticFilesToCopyStringSet[String(staticFileId)] = true;
         }
 
-        const loadStaticFilesArgs = constructors.newGetLoadStaticFilesArgs(serviceId, staticFilesToCopyStringSet);
+        const loadStaticFilesArgs: LoadStaticFilesArgs = newGetLoadStaticFilesArgs(serviceId, staticFilesToCopyStringSet);
         let request: grpc.requestCallback<LoadStaticFilesResponse>; // TODO (comment) need to initalize if want to change it to const
 
-    	let loadStaticFilesResp, err = this.client.loadStaticFiles(loadStaticFilesArgs, request); //TODO - need to find another way to do error checking ; (comment) need to initalize if want to change it to const
-    	if (err != null) {
+    	let loadStaticFilesResp, err = this.client.loadStaticFiles(loadStaticFilesArgs, request); //TODO - need to find another way to do error checking ; (comment) need to initalize if want to change it to const, type assertions
+    	if (err !== null) {
     		return [null, new Error ("An error occurred loading the requested static files into the namespace of service '%v'")]; //TODO - remove the erorr, and need to be throwing back the erorr
     	}
-    	const staticFileAbsFilepathsOnService = new Map(); //TODO (comment) = original type map[StaticFileID]string{}
+    	const staticFileAbsFilepathsOnService: Map<StaticFileID, string> = new Map();
     	for (let staticFileId in loadStaticFilesResp.CopiedStaticFileRelativeFilepaths) { //TODO should I change to const
-    		const filepathRelativeToExVolRoot = loadStaticFilesResp.CopiedStaticFileRelativeFilepaths[staticFileId];
-            const absFilepathOnContainer = path.Join(this.testVolumeMountpointOnServiceContainer, filepathRelativeToExVolRoot)
+    		const filepathRelativeToExVolRoot: string = loadStaticFilesResp.CopiedStaticFileRelativeFilepaths[staticFileId];
+            const absFilepathOnContainer: string = path.Join(this.testVolumeMountpointOnServiceContainer, filepathRelativeToExVolRoot)
     		staticFileAbsFilepathsOnService[<StaticFileID>(staticFileId)] = absFilepathOnContainer; //TODO - see if I'm typecasting correctly
     	}
-    	return [staticFileAbsFilepathsOnService, null] //TODO - Design decision, returning multiple values as an array
+    	return [staticFileAbsFilepathsOnService, null] 
 
     }
 }
