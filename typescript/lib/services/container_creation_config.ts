c574c884b9efdf622cc32bd8920c4bebd4570c65
Ali Syed <alisyed0206@gmail.com>
2021-08-02 00:45:43 -0400

    ported all of container config to typescript

---
+++ typescript/lib/services/container_creation_config.ts
@@ -0,0 +1,121 @@
+//import "os"
+
+const defaultKurtosisVolumeMountpoint = "/kurtosis-enclave-data";
+
+export type StaticFileID = string;
+
+// The ID of an artifact containing files that should be mounted into a service container
+export type FilesArtifactID = string;
+
+// ====================================================================================================
+//                                    Config Object
+// ====================================================================================================
+// TODO defensive copy when we're giving back complex objects?????
+// Docs available at https://docs.kurtosistech.com/kurtosis-libs/lib-documentation
+class ContainerCreationConfig {
+	
+    private readonly image: string;
+	private readonly kurtosisVolumeMountpoint: string;   // Technically the enclave data volume, but we call it this for simplicity for the user
+	private readonly usedPortsSet: Map<string, boolean>
+	private readonly fileGeneratingFuncs: Map<string, (func: number) => Error>; // TODO (comment) = file descriptors are just integers
+	private readonly usedStaticFilesSet: Map<StaticFileID, boolean>;
+	private readonly filesArtifactMountpoints: Map<FilesArtifactID, string>;
+
+    constructor(
+        image: string,
+        kurtosisVolumeMountpoint: string,
+        usedPortsSet: Map<string, boolean>,
+        fileGeneratingFuncs: Map<string, (func: number) => Error>,
+        usedStaticFilesSet: Map<StaticFileID, boolean>,
+        filesArtifactMountpoints: Map<FilesArtifactID, string>
+    ){
+        this.image = image;
+        this.kurtosisVolumeMountpoint = kurtosisVolumeMountpoint;
+        this.usedPortsSet = usedPortsSet;
+        this.fileGeneratingFuncs = fileGeneratingFuncs;
+        this.usedStaticFilesSet = usedStaticFilesSet;
+        this.filesArtifactMountpoints = filesArtifactMountpoints;
+    }
+
+    public getImage(): string {
+        return this.image;
+    }
+
+    public getKurtosisVolumeMountpoint(): string {
+        return this.kurtosisVolumeMountpoint;
+    }
+
+    public getUsedPortsSet(): Map<string, boolean> {
+        return this.usedPortsSet;
+    }
+
+    public getFileGeneratingFuncs(): Map<string, (func: number) => Error> {
+        return this.fileGeneratingFuncs;
+    }
+
+    public getFilesArtifactMountpoints(): Map<FilesArtifactID, string> {
+        return this.filesArtifactMountpoints;
+    }
+
+    public getUsedStaticFiles(): Map<StaticFileID, boolean> {
+        return this.usedStaticFilesSet;
+    }
+}
+
+// ====================================================================================================
+//                                        Builder
+// ====================================================================================================
+// TODO Defensive copies on all these With... functions???
+// Docs available at https://docs.kurtosistech.com/kurtosis-libs/lib-documentation
+class ContainerCreationConfigBuilder {
+	private image: string;
+	private kurtosisVolumeMountpoint: string;
+	private usedPortsSet: Map<string, boolean>;
+	private usedStaticFilesSet: Map<StaticFileID, boolean>;
+	private fileGeneratingFuncs: Map<string, (func: number) => Error>;
+	private filesArtifactMountpoints: Map<FilesArtifactID, string>;
+
+    constructor (image: string) {
+            this.image = image;
+            this.kurtosisVolumeMountpoint = defaultKurtosisVolumeMountpoint;
+            this.usedPortsSet = new Map(); //TODO (comment) = added type assertion above
+            this.fileGeneratingFuncs = new Map();
+            this.filesArtifactMountpoints = new Map();
+    }
+
+    public withKurtosisVolumeMountpoint(kurtosisVolumeMountpoint: string): ContainerCreationConfigBuilder {
+        this.kurtosisVolumeMountpoint = kurtosisVolumeMountpoint;
+        return this;
+    }
+
+    public withUsedPorts(usedPortsSet: Map<string, boolean>): ContainerCreationConfigBuilder {
+        this.usedPortsSet = usedPortsSet;
+        return this;
+    }
+
+    public withGeneratedFiles(fileGeneratingFuncs: Map<string, (func: number) => Error>): ContainerCreationConfigBuilder {
+        this.fileGeneratingFuncs = fileGeneratingFuncs;
+        return this;
+    }
+
+    public withStaticFiles(usedStaticFilesSet: Map<StaticFileID, boolean>): ContainerCreationConfigBuilder {
+        this.usedStaticFilesSet = usedStaticFilesSet;
+        return this;
+    }
+
+    public withFilesArtifacts(filesArtifactMountpoints: Map<FilesArtifactID, string>): ContainerCreationConfigBuilder {
+        this.filesArtifactMountpoints = filesArtifactMountpoints;
+        return this;
+    }
+
+    public build(): ContainerCreationConfig {
+        return new ContainerCreationConfig(
+            this.image,
+            this.kurtosisVolumeMountpoint,
+            this.usedPortsSet,
+            this.fileGeneratingFuncs,
+            this.usedStaticFilesSet,
+            this.filesArtifactMountpoints,
+        )
+    }
+}
---
+++ typescript/lib/services/container_run_config.ts
@@ -0,0 +1,74 @@
+// ====================================================================================================
+//                                    Config Object
+// ====================================================================================================
+// Docs available at https://docs.kurtosistech.com/kurtosis-libs/lib-documentation
+class ContainerRunConfig {
+	private readonly entrypointOverrideArgs: string[];
+	private readonly cmdOverrideArgs: string[];
+	private readonly environmentVariableOverrides: Map<string,string>;
+
+    constructor (
+        entrypointOverrideArgs: string[],
+        cmdOverrideArgs: string[],
+        environmentVariableOverrides: Map<string, string>,
+    ) {
+        this.entrypointOverrideArgs = entrypointOverrideArgs;
+        this.cmdOverrideArgs = cmdOverrideArgs;
+        this.environmentVariableOverrides = environmentVariableOverrides;
+    }
+
+    public getEntrypointOverrideArgs(): string[] {
+        return this.entrypointOverrideArgs;
+    }
+
+    public getCmdOverrideArgs(): string[] {
+        return this.cmdOverrideArgs;
+    }
+
+    public getEnvironmentVariableOverrides(): Map<string, string> {
+        return this.environmentVariableOverrides;
+    }
+}
+
+
+
+// ====================================================================================================
+//                                      Builder
+// ====================================================================================================
+// TODO Defensive copies on all these With... functions???
+// Docs available at https://docs.kurtosistech.com/kurtosis-libs/lib-documentation
+class ContainerRunConfigBuilder {
+	private entrypointOverrideArgs: string[];
+	private cmdOverrideArgs: string[];
+	private environmentVariableOverrides: Map<string, string>;
+
+    constructor () {
+        this.entrypointOverrideArgs = null;
+        this.cmdOverrideArgs = null;
+        this.environmentVariableOverrides = new Map();
+    }
+ 
+    public wthEntrypointOverride(args: string[]): ContainerRunConfigBuilder {
+        this.entrypointOverrideArgs = args;
+        return this;
+    }
+
+    public withCmdOverride(args: string[]): ContainerRunConfigBuilder {
+        this.cmdOverrideArgs = args;
+        return this;
+    }
+
+    public withEnvironmentVariableOverrides(envVars: Map<string, string>): ContainerRunConfigBuilder {
+        this.environmentVariableOverrides = envVars;
+        return this;
+    }
+
+    public build(): ContainerRunConfig {
+        return new ContainerRunConfig(
+            this.entrypointOverrideArgs,
+            this.cmdOverrideArgs,
+            this.environmentVariableOverrides,
+        );
+    }
+
+}
