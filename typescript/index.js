8d2f77e72a8886c41eb0dc08b8ef90900da0d9ab
Ali Syed <alisyed0206@gmail.com>
2021-08-13 08:54:28 -0400

    added the entry point file that holds all the exports so that users who import this package can access them

---
+++ typescript/index.js
@@ -0,0 +1,24 @@
+//Services
+export { StaticFileID, FilesArtifactID, ContainerCreationConfig } from './lib/services/container_creation_config'; //TODO - ContainerCreationConfigBuilder
+export { ContainerRunConfig } from './lib/services/container_run_config'; //TODO - ContainerRunConfigBuilder
+export { GeneratedFileFilepaths, ServiceContext } from './lib/services/service_context';
+export { ServiceID } from './lib/services/service';
+
+//Networks
+export { PartitionID } from './lib/networks/network_context'; //TODO - NetworkContext
+//TOOD - missing Network here
+
+//Modules
+export { LambdaID, LambdaContext } from './lib/modules/lambda_context';
+
+//Bulk Command Execution
+export { SchemaVersion } from './lib/bulk_command_execution/bulk_command_schema_version';
+export { V0SerializableCommand, V0BulkCommands } from './lib/bulk_command_execution/v0_bulk_command_api/v0_bulk_commands';
+export { V0CommandType, V0CommandTypeVisitor } from './lib/bulk_command_execution/v0_bulk_command_api/v0_command_types';
+//TODO - missing two files here
+
+//TODO Constructor Calls ?
+
+//Kurtosis Core RPC API Bindings
+export { ExecCommandArgs, GenerateFilesArgs, FileGenerationOptions, LoadStaticFilesArgs, LoadLambdaArgs, GetLambdaInfoArgs, RegisterStaticFilesArgs, RegisterFilesArtifactsArgs, GetServiceInfoArgs, PartitionServices, PartitionConnections, PartitionConnectionInfo, RegisterServiceArgs, StartServiceArgs, RemoveServiceArgs, RepartitionArgs, WaitForEndpointAvailabilityArgs, ExecuteBulkCommandsArgs, ExecuteLambdaArgs } from '../kurtosis_core_rpc_api_bindings/api_container_service_pb';
+export { loadLambda, executeLambda, getLambdaInfo, registerStaticFiles, registerFilesArtifactsregisterService, generateFiles, loadStaticFiles, startService, getServiceInfo, removeService, repartition, execCommand, waitForEndpointAvailability, executeBulkCommands } from './kurtosis_core_rpc_api_bindings/api_container_service_grpc_pb';
