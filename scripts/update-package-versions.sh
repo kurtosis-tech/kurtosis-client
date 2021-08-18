5b19e15487d08b68207b1a008b114abab29d8355
Ali Syed <alisyed0206@gmail.com>
2021-08-18 10:07:57 -0400

    created first draft for updating package version through release script

--- scripts/update-package-versions.sh
+++ scripts/update-package-versions.sh
@@ -9,11 +9,12 @@ script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # ==================================================================================================
 #                                             Constants
 # ==================================================================================================
-# TODO Fill constants here with UPPER_SNAKE_CASE, noting that the only variables constants may use are:
-# TODO  1) other constants (with the "${OTHER_CONSTANT}" syntax)
-# TODO  2) script_dirpath/root_dirpath from above
 LANG_ROOT_DIRPATH="$(dirname "${script_dirpath}")"
+
 SED_REPLACE_FILENAME_SUFFIX=".DELETEME"
+
+SPECIFIED_FILENAME="package.json"
+
 
 
 # ==================================================================================================
@@ -29,9 +30,6 @@ show_helptext_and_exit() {
 
 new_version="${1:-}"
 
-# TODO Modify this arg validation to match your arguments, keeping in mind:
-# TODO - Almost every arg should be verified to be non-empty
-# TODO - Filepath/dirpath ags often need to have their existence checked
 if [ -z "${new_version}" ]; then
     echo "Error: no new version provided" >&2
     show_helptext_and_exit
@@ -42,42 +40,32 @@ fi
 # ==================================================================================================
 #                                             Main Logic
 # ==================================================================================================
-# TODO implement your main logic here using the args & constants, keeping in mind that:
-# TODO - Non-constant variables should be lower_snake_case
-# TODO - Variables should be referenced like this: ${some_variable}
-# TODO - Capturing subprocess output should be done using $(), NOT backticks ``, like so:
-# TODO
-# TODO      my_variable="$(echo "Something")"
-# TODO
-# TODO - Every call should have its return value checked like so:
-# TODO
-# TODO       if ! some_command; then 
-# TODO           echo "Error: Some description of the error" >&2
-# TODO           exit 1
-# TODO       fi
+function getSearchReplace() {
+     value="${1:-}"
+     echo "\"version\": \"${value}\""
+}
 
 
-# Validate assumptions
 # Check that our package.json can be found in the right location
-if ! find $LANG_ROOT_DIRPATH -name "package.json"; then
-    echo "Error: No package.json file can be found inside '${LANG_ROOT_DIRPATH}'" >&2
+if ! find ${LANG_ROOT_DIRPATH} -name ${SPECIFIED_FILENAME}; then
+    echo "Error: No ${SPECIFIED_FILENAME} file can be found inside '${LANG_ROOT_DIRPATH}'" >&2
     exit 1
 fi
 
 #Check that the package.json contains exactly one line matching "version": "X.Y.Z"
-if ! grep "version: "X.Y.Z"" "$LANG_ROOT_DIRPATH/package.json"; then #TODO
-    echo "Error: Couldn't find version property in package.json" >&2
+if test $(grep -c $(getSearchReplace [0-9]\+\.[0-9]\+\.[0-9]\+) "${LANG_ROOT_DIRPATH}/${SPECIFIED_FILENAME}") -eq 1; then
+    echo "Error: Couldn't find one line matching \"version\": \"X.Y.Z\" in ${SPECIFIED_FILENAME}" >&2
     exit 1
 fi
 
 # Do the replacement on the file - in this case, on package.json
-if ! sed -i "${SED_REPLACE_FILENAME_SUFFIX}" 's/${X.Y.Z}/'${new_version}'/g' "package.json"; then
-    echo "Error: Replacing pattern '${search_pattern}' with value '${new_version}' in file package.json" >&2
+if ! sed -i "${SED_REPLACE_FILENAME_SUFFIX}" -E "s/$(getSearchReplace [0-9]\+\.[0-9]\+\.[0-9]\+)/$(getSearchReplace ${new_version})/g" ${SPECIFIED_FILENAME}; then #No magic values, but can I use regex in variables?
+    echo "Error: Replacing pattern '${search_pattern}' with value '${new_version}' in file ${SPECIFIED_FILENAME}" >&2
     exit 1
 fi
 
 # Delete the backup files that sed created
-if ! find "${root_dirpath}" -name "*${SED_REPLACE_FILENAME_SUFFIX}" -delete; then
+if ! find "${LANG_ROOT_DIRPATH}" -name "*${SED_REPLACE_FILENAME_SUFFIX}" -delete; then
     echo "Error: Couldn't delete the backup files that sed created ending in suffix '${SED_REPLACE_FILENAME_SUFFIX}'" > &2
     exit 1
 fi
