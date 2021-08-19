d210504d1a09d94d3ec16d7425ce4db0e8b92160
Ali Syed <alisyed0206@gmail.com>
2021-08-19 10:51:37 -0400

    iterated on Kevin's comments

--- typescript/scripts/build.sh
+++ typescript/scripts/build.sh
@@ -13,13 +13,5 @@ lang_root_dirpath="$(dirname "${script_dirpath}")"
 # # ==================================================================================================
 # #                                             Main Logic
 # # ==================================================================================================
-#find_results="$(find ${lang_root_dirpath} -name "*.ts" -not -path "${lang_root_dirpath}/kurtosis_core_rpc_api_bindings/*" -not -path "${lang_root_dirpath}/node_modules/*")"
-
-tsc ${lang_root_dirpath}/index.ts --outDir ${lang_root_dirpath}/build/ --downlevelIteration true --declaration true
-cp -R ${lang_root_dirpath}/kurtosis_core_rpc_api_bindings/ ${lang_root_dirpath}/build/kurtosis_core_rpc_api_bindings/ #TODO - error checking but after confirming if this is okay solution
-
-#tsc ${lang_root_dirpath}/index.ts --outDir ${lang_root_dirpath}/build --downlevelIteration
-
-#These two commands below made everything work
-#tsc index.ts --outDir ${lang_root_dirpath}build/ --downlevelIteration --declaration true --allowJs true
-#cp -R kurtosis_core_rpc_api_bindings/ build/kurtosis_core_rpc_api_bindings/
+tsc ${lang_root_dirpath}/index.ts --outDir ${lang_root_dirpath}/build/typescript/ --downlevelIteration true --declaration true
+cp -R ${lang_root_dirpath}/kurtosis_core_rpc_api_bindings ${lang_root_dirpath}/build/typescript/
