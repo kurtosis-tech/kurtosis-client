d78b2b9395a9adfd06ace4479cdd48786c1dd9d1
Ali Syed <alisyed0206@gmail.com>
2021-08-25 13:26:01 -0400

    updated the index.ts to only hold exports

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,39 +1,24 @@
-import { ContainerRunConfigBuilder } from "./lib/services/container_run_config";
-import { StaticFileID, FilesArtifactID, ContainerCreationConfigBuilder } from "./lib/services/container_creation_config";
-import { ServiceID } from "./lib/services/service";
-import { GeneratedFileFilepaths, ServiceContext } from "./lib/services/service_context";
-import { Network } from "./lib/networks/network";
-import { PartitionID, NetworkContext } from "./lib/networks/network_context";
-import { LambdaID, LambdaContext } from "./lib/modules/lambda_context";
-import { SchemaVersion } from "./lib/bulk_command_execution/bulk_command_schema_version";
-import { V0BulkCommands, V0SerializableCommand } from "./lib/bulk_command_execution/v0_bulk_command_api/v0_bulk_commands";
-import { V0CommandType, V0CommandTypeVisitor } from "./lib/bulk_command_execution/v0_bulk_command_api/v0_command_types";
-import { newExecCommandArgs, newGenerateFilesArgs, newFileGenerationOptions, newLoadStaticFilesArgs, newLoadLambdaArgs, newRegisterStaticFilesArgs, newRegisterFilesArtifactsArgs, newRegisterServiceArgs, newStartServiceArgs, newGetServiceInfoArgs, newRemoveServiceArgs, newPartitionServices, newRepartitionArgs, newPartitionConnections, newWaitForEndpointAvailabilityArgs, newExecuteBulkCommandsArgs, newExecuteLambdaArgs, newGetLambdaInfoArgs } from "./lib/constructor_calls";
-import { ApiContainerServiceClient } from "./kurtosis_core_rpc_api_bindings/api_container_service_grpc_pb";
-import { PartitionConnections, FileGenerationOptions, PortBinding } from "./kurtosis_core_rpc_api_bindings/api_container_service_pb";
-
 //Services
-export { StaticFileID, FilesArtifactID, ContainerCreationConfigBuilder }; 
-export { ContainerRunConfigBuilder };
-export { ServiceID };
-export { GeneratedFileFilepaths, ServiceContext };
+export { StaticFileID, FilesArtifactID, ContainerCreationConfigBuilder } from "./lib/services/container_creation_config"; 
+export { ContainerRunConfigBuilder } from "./lib/services/container_run_config";
+export { ServiceID } from "./lib/services/service";
+export { GeneratedFileFilepaths, ServiceContext } from "./lib/services/service_context";
 
 //Networks
-export { Network };
-export { PartitionID, NetworkContext };
+export { Network } from "./lib/networks/network";
+export { PartitionID, NetworkContext } from "./lib/networks/network_context";
 
 //Modules
-export { LambdaContext, LambdaID };
+export { LambdaContext, LambdaID } from "./lib/modules/lambda_context";
 
 //Bulk Command Execution
-export { SchemaVersion };
-export { V0BulkCommands, V0SerializableCommand };
-export { V0CommandType, V0CommandTypeVisitor };
-//TODO - missing three files here (bulk_command_serializer, service_ip_replacement, v0_bulk_command_test)
+export { SchemaVersion } from "./lib/bulk_command_execution/bulk_command_schema_version";
+export { V0BulkCommands, V0SerializableCommand } from "./lib/bulk_command_execution/v0_bulk_command_api/v0_bulk_commands";
+export { V0CommandType, V0CommandTypeVisitor } from "./lib/bulk_command_execution/v0_bulk_command_api/v0_command_types";;
 
 //Constructor Calls
-export { newExecCommandArgs, newGenerateFilesArgs, newFileGenerationOptions, newLoadStaticFilesArgs, newLoadLambdaArgs, newRegisterStaticFilesArgs, newRegisterFilesArtifactsArgs, newRegisterServiceArgs, newStartServiceArgs, newGetServiceInfoArgs, newRemoveServiceArgs, newPartitionServices, newRepartitionArgs, newPartitionConnections, newWaitForEndpointAvailabilityArgs, newExecuteBulkCommandsArgs, newExecuteLambdaArgs, newGetLambdaInfoArgs };
+export { newExecCommandArgs, newGenerateFilesArgs, newFileGenerationOptions, newLoadStaticFilesArgs, newLoadLambdaArgs, newRegisterStaticFilesArgs, newRegisterFilesArtifactsArgs, newRegisterServiceArgs, newStartServiceArgs, newGetServiceInfoArgs, newRemoveServiceArgs, newPartitionServices, newRepartitionArgs, newPartitionConnections, newWaitForEndpointAvailabilityArgs, newExecuteBulkCommandsArgs, newExecuteLambdaArgs, newGetLambdaInfoArgs } from "./lib/constructor_calls";
 
 //Kurtosis Core RPC API Bindings
-export { ApiContainerServiceClient };
-export { PartitionConnections, FileGenerationOptions, PortBinding };
+export { ApiContainerServiceClient } from "./kurtosis_core_rpc_api_bindings/api_container_service_grpc_pb";
+export { PartitionConnections, FileGenerationOptions, PortBinding } from "./kurtosis_core_rpc_api_bindings/api_container_service_pb";
