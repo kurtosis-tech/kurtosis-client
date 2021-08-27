abf003b4e4466af283d7816b96b537548a46077f
Ali Syed <alisyed0206@gmail.com>
2021-08-27 08:35:26 -0400

    Pre-release changes for version 0.13.5

--- golang/lib/kurtosis_api_version_const/kurtosis_api_version_const.go
+++ golang/lib/kurtosis_api_version_const/kurtosis_api_version_const.go
@@ -2,5 +2,5 @@ package kurtosis_api_version_const
 
 const (
 	// DO NOT UPDATE THIS VALUE! It will get updated automatically during the release process
-	KurtosisApiVersion = "0.13.4"
+	KurtosisApiVersion = "0.13.5"
 )
